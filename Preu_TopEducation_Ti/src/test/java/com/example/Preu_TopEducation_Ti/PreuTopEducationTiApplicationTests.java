7a9c1bcb6cb2500ccbb699522ab68d3689e0ad9e
Alejandro Aldea <alejandro.aldea@usach.cl>
2023-09-21 01:13:20 -0300

    Creación del proyecto

---
+++ Preu_TopEducation_Ti/src/test/java/com/example/Preu_TopEducation_Ti/PreuTopEducationTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Preu_TopEducation_Ti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreuTopEducationTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
